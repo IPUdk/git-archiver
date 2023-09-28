0d59881ed032b51b368e910c70373b2359c3e9fe
Albert Seligmann <ase@ipu.dk>
2023-09-28 15:42:23 +0200

    Get token and organization name from environment

--- github_archiver.py
+++ github_archiver.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import time
 
@@ -13,3 +14,7 @@ logging.basicConfig(
 )
 logging.Formatter.converter = time.gmtime  # Use UTC time
 log = logging.getLogger()  # Get root logger
+
+access_token = os.environ.get("GITHUB_API_TOKEN")  # Needs "repo", "admin:org" scope
+org_name = os.environ.get("GITHUB_ORG_NAME")  # Get the organization name
+
