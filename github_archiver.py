e11c4505a8ef0bd661bb5432d343b6139369ea4d
Albert Seligmann <ase@ipu.dk>
2023-09-28 15:45:39 +0200

    Add function to get list of repos in org

--- github_archiver.py
+++ github_archiver.py
@@ -1,6 +1,8 @@
 import os
 import logging
 import time
+import requests
+import json
 
 
 # Setup logging
@@ -18,3 +20,50 @@ log = logging.getLogger()  # Get root logger
 access_token = os.environ.get("GITHUB_API_TOKEN")  # Needs "repo", "admin:org" scope
 org_name = os.environ.get("GITHUB_ORG_NAME")  # Get the organization name
 
+
+def get_repos():
+    """
+    Function to get the list of repositories for the organization
+    """
+
+    log.info(f"Getting list of repos in organization \"{org_name}\"...")
+    # Define endpoint
+    endpoint = f"https://api.github.com/orgs/{org_name}/repos"
+    # Define headers
+    headers = {"Accept": "application/vnd.github+json",
+               "Authorization": f"Bearer {access_token}"}
+    # Initialize the output list
+    repos = []
+
+    # Paginate through the results
+    page = 1
+    per_page = 100
+    while True:
+        # Get page
+        log.info(f"  Getting page {page} (page size: {per_page}) of repos...")
+        response = requests.get(endpoint,
+                                headers=headers,
+                                params={"per_page": per_page, 
+                                        "page": page})
+        # Check response
+        if response.status_code != 200:
+            log.error("  Got non-200 response!")
+            break
+        # Convert JSON response to python object
+        response_list = json.loads(response.text)
+        # Add repos to list
+        repos.extend(response_list)
+        # Check if there are no more repos to fetch
+        if len(response_list) < per_page:
+            break
+        # Increment page
+        page += 1
+
+    log.info(f"  Got {len(repos)} repos")
+
+    return repos
+
+# Loop through the list of repositories and create a migration archive for each one
+repos = get_repos()
+for repo in repos:
+    print(f"{repo['id']}: {repo['name']}")
