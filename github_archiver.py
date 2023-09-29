6788b1b36fb62238a275541f2ae95a44a8b0dd71
Albert Seligmann <ase@ipu.dk>
2023-09-29 13:46:58 +0200

    Format

--- github_archiver.py
+++ github_archiver.py
@@ -26,12 +26,14 @@ def get_repos():
     Get a list of repositories under the organization
     """
 
-    log.info(f"Getting list of repos in organization \"{org_name}\"...")
+    log.info(f'Getting list of repos in organization "{org_name}"...')
     # Define endpoint
     endpoint = f"https://api.github.com/orgs/{org_name}/repos"
     # Define headers
-    headers = {"Accept": "application/vnd.github+json",
-               "Authorization": f"Bearer {access_token}"}
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {access_token}",
+    }
     # Initialize the output list
     repos = []
 
@@ -41,13 +43,14 @@ def get_repos():
     while True:
         # Get page
         log.info(f"  Getting page {page} (page size: {per_page}) of repos...")
-        response = requests.get(endpoint,
-                                headers=headers,
-                                params={"per_page": per_page, 
-                                        "page": page})
+        response = requests.get(
+            endpoint, headers=headers, params={"per_page": per_page, "page": page}
+        )
         # Check response
         if response.status_code != 200:
-            log.error(f"Error getting repositories: status code: {response.status_code}, text: {response.text}")
+            log.error(
+                f"Error getting repositories: status code: {response.status_code}, text: {response.text}"
+            )
             break
         # Convert JSON response to python object
         response_obj = json.loads(response.text)  # List of dicts
@@ -72,12 +75,16 @@ def create_migration_export(repo):
     # Define endpoint
     endpoint = f"https://api.github.com/orgs/{org_name}/migrations"
     # Define headers
-    headers = {"Accept": "application/vnd.github+json",
-               "Authorization": f"Bearer {access_token}"}
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {access_token}",
+    }
     # Define data
-    data = {"lock_repositories": True,  # Must be manually unlocked!
-            "lock_reason": "migrating",
-            "repositories": [f"{org_name}/{repo['name']}"]}
+    data = {
+        "lock_repositories": True,  # Must be manually unlocked!
+        "lock_reason": "migrating",
+        "repositories": [f"{org_name}/{repo['name']}"],
+    }
     # Make request
     response = requests.post(
         endpoint,
@@ -86,7 +93,9 @@ def create_migration_export(repo):
     )
     # Check response
     if response.status_code != 201:
-        log.error(f"Error creating migration for {repo['name']}: status code: {response.status_code}, text: {response.text}")
+        log.error(
+            f"Error creating migration for {repo['name']}: status code: {response.status_code}, text: {response.text}"
+        )
         return None
     # Convert JSON response to python object
     response_obj = json.loads(response.text)  # Dict
@@ -105,8 +114,10 @@ def get_migration_status(migration_id):
     # Define endpoint
     endpoint = f"https://api.github.com/orgs/{org_name}/migrations/{migration_id}"
     # Define headers
-    headers = {"Accept": "application/vnd.github+json",
-               "Authorization": f"Bearer {access_token}"}
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {access_token}",
+    }
     # Make request
     response = requests.get(
         endpoint,
@@ -114,13 +125,17 @@ def get_migration_status(migration_id):
     )
     # Check response
     if response.status_code != 200:
-        log.error(f"Error getting migration status for migration_id={migration_id}: status code: {response.status_code}, text: {response.text}")
+        log.error(
+            f"Error getting migration status for migration_id={migration_id}: status code: {response.status_code}, text: {response.text}"
+        )
         return None
     # Convert JSON response to python object
     response_obj = json.loads(response.text)  # Dict
     migration_status = response_obj["state"]
 
-    log.info(f"  Migration status for migration_id={migration_id} is {migration_status}")
+    log.info(
+        f"  Migration status for migration_id={migration_id} is {migration_status}"
+    )
 
     return migration_status
 
@@ -133,16 +148,17 @@ def unlock_repository(repo):
     # Define endpoint
     endpoint = f"https://api.github.com/orgs/{org_name}/migrations/{repo['migration_id']}/repos/{repo['name']}/lock"
     # Define headers
-    headers = {"Accept": "application/vnd.github+json",
-               "Authorization": f"Bearer {access_token}"}
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {access_token}",
+    }
     # Make request
-    response = requests.delete(
-        endpoint,
-        headers=headers
-    )
+    response = requests.delete(endpoint, headers=headers)
     # Check response
     if response.status_code != 204:
-        log.error(f"Error unlocking {repo['name']}: status code: {response.status_code}, text: {response.text}")
+        log.error(
+            f"Error unlocking {repo['name']}: status code: {response.status_code}, text: {response.text}"
+        )
         return
 
     log.info(f"  Successfully unlocked {repo['name']}")
@@ -153,12 +169,16 @@ def get_commit_sha(repo, ref=""):
     Get the commit SHA for a repository reference (defaults to the default branch)
     """
     specific_ref = ref != ""
-    log.info(f"Getting the commit SHA for {repo['name']}{' at ' + ref if specific_ref else ''}...")
+    log.info(
+        f"Getting the commit SHA for {repo['name']}{' at ' + ref if specific_ref else ''}..."
+    )
     # Define endpoint
     endpoint = f"https://api.github.com/repos/{org_name}/{repo['name']}/commits{'/' + ref if specific_ref else ''}"
     # Define headers
-    headers = {"Accept": "application/vnd.github+json",
-               "Authorization": f"Bearer {access_token}"}
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {access_token}",
+    }
     # Make request
     response = requests.get(
         endpoint,
@@ -166,17 +186,23 @@ def get_commit_sha(repo, ref=""):
     )
     # Check response
     if response.status_code != 200:
-        log.error(f"Error getting the commit SHA: status code: {response.status_code}, text: {response.text}")
+        log.error(
+            f"Error getting the commit SHA: status code: {response.status_code}, text: {response.text}"
+        )
         return None
     # Convert JSON response to python object
     sha = ""
-    response_obj = json.loads(response.text)  # Dict or list of dicts depending on if a specific ref was provided
+    response_obj = json.loads(
+        response.text
+    )  # Dict or list of dicts depending on if a specific ref was provided
     if specific_ref:
         sha = response_obj["sha"]
     else:
         sha = response_obj[0]["sha"]
 
-    log.info(f"  Commit SHA for {repo['name']} at {'default' if ref == '' else ref} is {sha}")
+    log.info(
+        f"  Commit SHA for {repo['name']} at {'default' if ref == '' else ref} is {sha}"
+    )
 
     return sha
 
@@ -192,10 +218,12 @@ def download_file(file_name, url, headers={}):
         os.makedirs(dir, exist_ok=True)
     # Make request and download file
     try:
-        with requests.get(url, headers=headers, stream=True) as r: # Streaming
+        with requests.get(url, headers=headers, stream=True) as r:  # Streaming
             r.raise_for_status()
-            with open(file_name, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=8192): # Iterate over streamed chunks
+            with open(file_name, "wb") as f:
+                for chunk in r.iter_content(
+                    chunk_size=8192
+                ):  # Iterate over streamed chunks
                     f.write(chunk)
     except Exception as e:
         log.error(f"Error downloading {url}: {e}")
@@ -220,8 +248,10 @@ def download_migration_export(repo, file_path):
     # Define endpoint
     endpoint = f"https://api.github.com/orgs/{org_name}/migrations/{repo['migration_id']}/archive"
     # Define headers
-    headers = {"Accept": "application/vnd.github+json",
-               "Authorization": f"Bearer {access_token}"}
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {access_token}",
+    }
     # Make request
     log.info(f"Getting migration archive download URL for {repo['name']}...")
     response = requests.get(
@@ -230,13 +260,17 @@ def download_migration_export(repo, file_path):
     )
     # Check response
     if response.status_code != 302 and response.status_code != 200:
-        log.error(f"Error downloading migration archive for {repo['name']}: status code: {response.status_code}, text: {response.text}")
+        log.error(
+            f"Error downloading migration archive for {repo['name']}: status code: {response.status_code}, text: {response.text}"
+        )
 
     # Construct file name
     file_name = os.path.join(file_path, f"export_{repo['name']}_{repo['id']}.tar.gz")
     log.info(f"  Downloading migration archive for {repo['name']} to {file_name}...")
     # Get file
-    download_file(file_name, response.url, headers={}) # <-- Download URL is temporary and requires empty headers
+    download_file(
+        file_name, response.url, headers={}
+    )  # <-- Download URL is temporary and requires empty headers
     log.info(f"  Migration archive downloaded for {repo['name']}")
 
 
@@ -252,20 +286,26 @@ def download_project(repo, file_path, ref=""):
         return None
 
     specific_ref = ref != ""
-    
+
     # Define endpoint
     url = f"https://api.github.com/repos/{org_name}/{repo['name']}/tarball/{ref if specific_ref else ''}"
     # Define headers
-    headers = {"Accept": "application/vnd.github+json",
-               "Authorization": f"Bearer {access_token}"}
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {access_token}",
+    }
 
     # Construct file name
-    file_name = os.path.join(file_path, f"repository_archive_{repo['name']}_{repo['id']}_{repo['sha']}.tar.gz")
+    file_name = os.path.join(
+        file_path,
+        f"repository_archive_{repo['name']}_{repo['id']}_{repo['sha']}.tar.gz",
+    )
     log.info(f"  Downloading project archive for {repo['name']} to {file_name}...")
     # Get file
-    download_file(file_name, url, headers=headers) # <-- Requires authentication through headers due to private repositories
+    download_file(
+        file_name, url, headers=headers
+    )  # <-- Requires authentication through headers due to private repositories
     log.info(f"  Project archive downloaded for {repo['name']}")
-
 
 
 # Loop through the list of repositories and create a migration archive for each one
@@ -273,8 +313,7 @@ repos = []
 try:
     repos_raw = get_repos()
     for repo in repos_raw:
-        r = {"id": repo["id"],
-            "name": repo["name"]}
+        r = {"id": repo["id"], "name": repo["name"]}
         r["sha"] = get_commit_sha(r)
         r["migration_id"] = create_migration_export(r)
         repos.append(r)
