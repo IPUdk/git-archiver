2c39c453fef0cececeb651969f21d1f4ccf9d6d7
Albert Seligmann <ase@ipu.dk>
2023-10-24 13:38:41 +0200

    Add main guard clause

--- github_archiver.py
+++ github_archiver.py
@@ -5,22 +5,6 @@ import requests
 import json
 
 
-# Setup logging
-logging.basicConfig(
-    level=logging.INFO,
-    format="UTC %(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        logging.FileHandler("github_archiver.log"),  # Log to file
-        logging.StreamHandler(),  # Log to console
-    ],
-)
-logging.Formatter.converter = time.gmtime  # Use UTC time
-log = logging.getLogger()  # Get root logger
-
-access_token = os.environ.get("GITHUB_API_TOKEN")  # Needs "repo", "admin:org" scope
-org_name = os.environ.get("GITHUB_ORG_NAME")  # Get the organization name
-
-
 def get_repos():
     """
     Get a list of repositories under the organization
@@ -308,22 +292,39 @@ def download_project(repo, file_path, ref=""):
     log.info(f"  Project archive downloaded for {repo['name']}")
 
 
-# Loop through the list of repositories and create a migration archive for each one
-repos = []
-try:
-    repos_raw = get_repos()
-    for repo in repos_raw:
-        r = {"id": repo["id"], "name": repo["name"]}
-        r["sha"] = get_commit_sha(r)
-        r["migration_id"] = create_migration_export(r)
-        repos.append(r)
+if __name__ == "__main__":
+    # Setup logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format="UTC %(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler("github_archiver.log"),  # Log to file
+            logging.StreamHandler(),  # Log to console
+        ],
+    )
+    logging.Formatter.converter = time.gmtime  # Use UTC time
+    log = logging.getLogger()  # Get root logger
 
-    log.info(f"Processing repositories: {repos}")
+    access_token = os.environ.get("GITHUB_API_TOKEN")  # Needs "repo", "admin:org" scope
+    org_name = os.environ.get("GITHUB_ORG_NAME")  # Get the organization name
 
-    for repo in repos:
-        download_migration_export(repo, "exports/github")
-        download_project(repo, "exports/github")
+    # Loop through the list of repositories and create a migration archive for each one
+    repos = []
+    try:
+        repos_raw = get_repos()
+        for repo in repos_raw:
+            r = {"id": repo["id"], "name": repo["name"]}
+            r["sha"] = get_commit_sha(r)
+            r["migration_id"] = create_migration_export(r)
+            repos.append(r)
 
-finally:
-    for repo in repos:
-        unlock_repository(repo)
+        log.info(f"Processing repositories: {repos}")
+
+        for repo in repos:
+            download_migration_export(repo, "exports/github")
+            download_project(repo, "exports/github")
+
+    finally:
+        for repo in repos:
+            unlock_repository(repo)
+    log.info("Done!")
